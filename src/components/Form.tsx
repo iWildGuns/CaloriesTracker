61cd4c030994002b382a20733245f68081796656
iWildGuns <hrdr1995@gmail.com>
2024-11-08 07:09:44 -0300

    Updating first steps of my project

---
+++ src/components/Form.tsx
@@ -0,0 +1,73 @@
+import { useState, ChangeEvent } from "react";
+import { categories } from "../data/categories";
+
+export default function Form() {
+  const [activity, setActivity] = useState({
+    category: 1,
+    name: "",
+    calories: 0,
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  return (
+    <form className=" space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className=" font-bold">
+          Category:
+        </label>
+        <select
+          name=""
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+          value={activity.category}
+          onChange={handleChange}
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="name" className=" font-bold">
+          Activity:
+        </label>
+        <input
+          id="name"
+          type="text"
+          className="border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Food, Orange Juice, Salad, Exercises, Weights, Spining "
+          value={activity.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className=" font-bold">
+          Calories:
+        </label>
+        <input
+          id="calories"
+          type="number"
+          className="border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. 300 or 500 "
+          value={activity.calories}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        className=" bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value="Save food or save exercises..."
+      />
+    </form>
+  );
+}
