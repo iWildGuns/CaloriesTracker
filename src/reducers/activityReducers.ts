51e27b45b289695833de661111ae38f428fe65db
iWildGuns <hrdr1995@gmail.com>
2024-11-22 02:04:47 -0300

    another commit

--- src/reducers/activityReducers.ts
+++ src/reducers/activityReducers.ts
@@ -52,7 +52,6 @@ export const activityReducer = (
   try {
     if (action.type === "deleteActivity") {
       return {
-        // ...state,
         activities: state.activities.filter(
           (activity) => activity.id !== action.payload.id
         ),
