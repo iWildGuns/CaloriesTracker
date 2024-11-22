b01ce8b204db310c606fbf3fd1c18b6b0b6af0c6
iWildGuns <hrdr1995@gmail.com>
2024-11-22 01:53:20 -0300

    fixing bugs

--- src/reducers/activityReducers.ts
+++ src/reducers/activityReducers.ts
@@ -49,13 +49,17 @@ export const activityReducer = (
     };
   }
 
-  if (action.type === "deleteActivity") {
-    return {
-      ...state,
-      activities: state.activities.filter(
-        (activity) => activity.id !== action.payload.id
-      ),
-    };
+  try {
+    if (action.type === "deleteActivity") {
+      return {
+        // ...state,
+        activities: state.activities.filter(
+          (activity) => activity.id !== action.payload.id
+        ),
+      };
+    }
+  } catch (error) {
+    console.log(error);
   }
 
   if (action.type === "restartApp") {
